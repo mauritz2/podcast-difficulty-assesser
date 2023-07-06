77bf7659f715bd495129b1a84898d8015e1b534c
Fredrik Uddsten <12560602+mauritz2@users.noreply.github.com>
2023-07-06 17:57:48 +0200

    basic word commonality metric implemented

---
+++ assess_difficulty.py
@@ -0,0 +1,39 @@
+
+COMMON_WORDS_FILE = "assets/top_1000_french_words.csv"
+
+
+def get_words_in_transcript():
+    # Temporary test func
+    with open("french_podcast_transcript.txt", "r") as f:
+        data = f.read()
+        print(data)
+    words = data.split(" ")
+    return words
+
+
+def get_common_wordlist():
+    # TODO - should ideally be able to account for plurals, feminine forms, verb conjugations
+    with open(COMMON_WORDS_FILE, "r") as f:
+        wordlist_raw = f.readlines()
+        wordlist = list(map(lambda x: x.strip(), wordlist_raw))
+    return wordlist
+
+
+def assess_degree_of_uncommon_words(transcript_words:str, wordlist:list[str]):
+    total_words = len(transcript_words)
+    uncommon_words = 0
+
+    for word in transcript_words:
+        if word not in wordlist:
+            uncommon_words += 1
+    
+    pct_uncommon_words = uncommon_words / total_words
+    return pct_uncommon_words
+
+     
+wordlist = get_common_wordlist()
+transcript = get_words_in_transcript()
+
+pct_uncommon_words = assess_degree_of_uncommon_words(transcript, wordlist)
+
+print(pct_uncommon_words)
--- transcriber.py
+++ transcriber.py
@@ -2,17 +2,22 @@ import speech_recognition as sr
 from os import path
 from pydub import AudioSegment
 
-# convert mp3 file to wav                                                       
-sound = AudioSegment.from_mp3("french_podcast_2.mp3")
-sound.export("transcript.wav", format="wav")
+# convert mp3 file to wav
+PODCAST_FILENAME_BASE = "french_podcast"
+PODCAST_FILENAME_MP3 = PODCAST_FILENAME_BASE + ".mp3" 
+PODCAST_FILENAME_WAV = PODCAST_FILENAME_BASE + ".wav"
+PODCAST_FILENAME_TRANSCRIPT = PODCAST_FILENAME_BASE + "_transcript.txt"
 
-
-# transcribe audio file                                                         
-AUDIO_FILE = "transcript.wav"
+sound = AudioSegment.from_mp3(PODCAST_FILENAME_MP3)
+sound.export(PODCAST_FILENAME_WAV, format="wav")
 
 # use the audio file as the audio source                                        
 r = sr.Recognizer()
-with sr.AudioFile(AUDIO_FILE) as source:
+with sr.AudioFile(PODCAST_FILENAME_WAV) as source:
         audio = r.record(source)  # read the entire audio file                  
 
-        print("Transcription: " + r.recognize_google(audio, language="fr-FR"))
+        with open(PODCAST_FILENAME_TRANSCRIPT, "w+") as transcript_f:
+                transcript = r.recognize_google(audio, language="fr-FR")
+                print(transcript)
+                transcript_f.write(transcript)
+                print("Writing to file complete")
